670bd701c92ebb98cf226fb31c7a9e8dd1a41c7e
yiruiduan <yiruiduan@126.com>
2019-01-21 14:02:32 +0800

    分页显示

--- new/views.py
+++ new/views.py
@@ -26,26 +26,72 @@ def tpl4(request):
     return render(request,"new/tpl4.html")
 
 USER_LIST=[]
-for i in range(101):
+for i in range(1009):
     USER_LIST.append(i)
 def user_list(request):
-    arg_num=20
+    per_num=10
+    mark_per_page = 11
+
     page_num=request.GET.get("p","1")
     page_num=int(page_num)
-    start_num=(page_num-1)*arg_num
-    end_num=page_num*arg_num
+    start_num=(page_num-1)*per_num
+    end_num=page_num*per_num
     user=USER_LIST[start_num:end_num]
     user_total=len(USER_LIST)
-    count,y=divmod(user_total,arg_num)
+    total_count,y=divmod(user_total,per_num)
     if y:
-        count+=1
+        total_count+=1
     page_str=[]
-    for i in range(1,count+1):
-        if i == int(page_num):
-            tmep='<a class="page_sty active" href="/new/user_list/?p=%s">%s</a>' %(i,i)
+    # 上一页
+    if page_num <=1:
+        prev= '<a class="page_sty" href="javascript:void(0)">上一页</a>'
+    else:
+        prev = '<a class="page_sty" href="/new/user_list?p=%s">上一页</a>'%(page_num-1)
+    page_str.append(prev)
+
+    # 显示的页签
+    if total_count <= mark_per_page:
+        start_index = 1
+        end_index = total_count +1
+    else:
+        if page_num <= (mark_per_page+1)/2 :
+            start_index = 1
+            end_index = 11
         else:
-            tmep='<a class="page_sty" href="/new/user_list/?p=%s">%s</a>' %(i,i)
+            if page_num+(mark_per_page+1)/2 > total_count:
+                end_index= total_count+1
+                start_index= total_count -11 +1
+            else:
+                start_index = page_num - (mark_per_page -1)/2
+                end_index = page_num +(mark_per_page +1)/2
+
+    for i in range(int(start_index),int(end_index)):
+        if i == int(page_num):
+            tmep='<a class="page_sty active" href="/new/user_list?p=%s">%s</a>' %(i,i)
+        else:
+            tmep='<a class="page_sty" href="/new/user_list?p=%s">%s</a>' %(i,i)
         page_str.append(tmep)
+    # 下一页
+    if page_num >= total_count:
+        nex= '<a class="page_sty" href="javascript:void(0)">下一页</a>'
+    else:
+        nex= '<a class="page_sty" href="/new/user_list?p=%s">下一页</a>'%(page_num+1)
+    page_str.append(nex)
+    # 跳转
+    jump ="""
+        <input style="width: 28px;height: 28px;border-radius: 5px" type="text"/><a class="page_sty" onclick="jumpTo(this,'new/user_list?p=');" id="jump_to">跳转到</a>
+        <script>
+            function jumpTo(ths,base) {
+                var max_page = %d;
+                var val=document.getElementById("jump_to").previousSibling.value;
+                if( val > max_page){
+                    val =max_page;
+                };
+                if (val < 1){val =1};
+                location.href = base+ val;
+            }
+        </script>    """ %total_count
+    page_str.append(jump)
     page_str="".join(page_str)
     page_str=mark_safe(page_str)
     return render(request,"new/user_list.html",{"li":user,"page_str":page_str})
